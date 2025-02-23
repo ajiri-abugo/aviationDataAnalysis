5587e82895c701c989a360f0803be1458626dcdb
ajiri-abugo <aajirioghene@gmail.com>
2025-02-23 15:27:15 +0100

    updated mongo connection

--- config/mongoconnect.py
+++ config/mongoconnect.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import pymongo
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
@@ -7,21 +8,27 @@ from urllib.parse import quote_plus
 load_dotenv()
 
 # MongoDB connection details
-host = "127.0.0.1"
-port = 27017
+host = os.getenv('MONGO_HOST')
+port = os.getenv('MONGO_PORT')
 username = os.getenv('MONGO_USER')
 password = os.getenv('MONGO_PWD')
-database = "travelagency"
+database = os.getenv('MONGO_DB')
+auth_db = os.getenv('MONGO_AUTH_DB')
 
 # Encode password to handle special characters
 encoded_password = quote_plus(password)
 
 # Construct MongoDB connection URI
-uri = f"mongodb://{username}:{encoded_password}@{host}:{port}/{database}?authSource=admin"
-#connecturl = "mongodb://{}:{}@{}:27017/?authSource=admin".format(username,password,host)
+uri = f"mongodb://{username}:{encoded_password}@{host}:{port}/{database}?authSource={auth_db}"
 
 # Create a function to return the database connection
 def get_mongo_db():
-    client = MongoClient(uri)
-    db = client[database]
-    return db, client  # Returns the database object and client
+    try:
+        client = MongoClient(uri)
+        db = client[database]
+        return db, client  # Returns the database object and client
+    except pymongo.errors.ConnectionFailure as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    get_mongo_db()
